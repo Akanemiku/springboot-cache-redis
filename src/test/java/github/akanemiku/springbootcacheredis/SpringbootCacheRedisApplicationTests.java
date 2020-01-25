f77fae281891be438a32bb2113206ae07927991a
Akanemiku <806394913@qq.com>
2020-01-25 16:54:48 +0800

    -init-

---
+++ src/test/java/github/akanemiku/springbootcacheredis/SpringbootCacheRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package github.akanemiku.springbootcacheredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCacheRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
